ac28440f64d5d9b2a174aa473d741b7bff21fd69
Sai Khurana <sai.khurana@ashoka.edu.in>
2024-06-20 16:48:19 +0530

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@ Mux *hdmiMuxArray[3] = {
     new Mux(18, 19, 500),
     new Mux(9, 10, 500)};
 
-Task t0(TASK_IMMEDIATE,TASK_FOREVER,[](){ hdmiMuxArray[0]->runtime(); });
-Task t1(TASK_IMMEDIATE,TASK_FOREVER,[](){ hdmiMuxArray[1]->runtime(); });
-Task t2(TASK_IMMEDIATE,TASK_FOREVER,[](){ hdmiMuxArray[2]->runtime(); });
-
 Task *tasks[] = {
     new Task(TASK_IMMEDIATE,TASK_FOREVER,[](){ hdmiMuxArray[0]->runtime(); }),
     new Task(TASK_IMMEDIATE,TASK_FOREVER,[](){ hdmiMuxArray[1]->runtime(); }),
@@ -44,7 +40,7 @@ void setup()
     }
     userScheduler->addTask(*driver_task);
     driver_task->enableDelayed(3000);
-    
+
     userScheduler->startNow();
 }
 
