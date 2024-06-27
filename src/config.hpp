42a642fe194d0227b0881727e3df815aa7f30f49
Sai Khurana <44598300+saikhurana98@users.noreply.github.com>
2024-06-27 13:21:05 +0530

    Cleanup and docs (#4)
    * add docs config class
    
    * adds docs for mux classs
    
    * add docs for hdmihandler class
    
    * add docs for SerialHandler class
    
    * update docs for config class

--- src/config.hpp
+++ src/config.hpp
@@ -27,20 +27,74 @@ enum RestoreMode
     CUSTOM
 };
 
+/**
+ * @brief Map of Channel id and pinout pair (trig,sense)
+ * 
+ */
 typedef std::map<String, pair<int, int>> HdmiChannelPinoutMap;
+
+/**
+ * @brief Map of Channel id and Hdmi Source enum
+ * 
+ */
 typedef std::map<String, HdmiSource> HdmiChannelSourceMap;
 
 
+/**
+ * @brief Main Application Configuration Class 
+ * 
+ * This must contain all user configurable parameters ( Pinout, baud_rate, default mux behaviour, etc)
+ * 
+ */
 class Config
 {
 public:
+    /**
+     * @brief Serial Configuration Parameters
+    */
     SerialConfig serialConfig;
+
+    /**
+     * @brief Reference to a map of Channel id to pinout
+     */
     HdmiChannelPinoutMap *hdmiChannelPinouts;
+
+    /**
+     * @brief State to set the Mux's to on boot. Loaded from flash on boot
+     */
     HdmiChannelSourceMap restoreState;
+
+
+    /**
+     * @brief Current Restore Mode. 
+     * NONE : Restored to HDMI2
+     * CUSTOM : Custom Restore state from flash
+     * RESTORE_PREVIOUS : Restored to the last known boot state
+     */
     RestoreMode currentRestoreMode;
+
+    /**
+     * @brief Sets the on-boot RestoreMode & Source Map in flash.
+     * 
+     * @param mode RestoreMode ( NONE, CUSTOM, RESTORE_PREVIOUS )
+     * @param map Source Map ( Only needed when RestoreMode::Custom )
+     */
     void setRestoreState(RestoreMode mode, HdmiChannelSourceMap map);
-    void addHdmiPinout(String channelId, int trigPin, int sensePin);
+    /** 
+     * @brief Loads the Saved config from flash or loads default and saves it
+    */
     void init();
+    /**
+     * @brief Construct a new Config object
+     * 
+     *  Contains all the configuration variables needed to construct all dependent classes.
+     * 
+     * @param serialRxPin Tx Pin of the Control Serial Interface
+     * @param serialTxPin Tx Pin of the Control Serial Interface
+     * @param baud Serial transmit Baud rate 
+     * @param pinout HdmiChannelPinMap Map of the channel id and the pinout pair (trig, sense)
+     * @param restoreMode Default Restore Mode if the config is not present
+     */
     Config(int serialRxPin, int serialTxPin, double baud, HdmiChannelPinoutMap &pinout, RestoreMode restoreMode);
 private:
     String flashConfigFilename = "/config.json";
